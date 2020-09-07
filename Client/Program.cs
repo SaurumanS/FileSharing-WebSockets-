6286b8072b6a30298ff591ce43cb8bdaa024418d
Saliy Alexander <isaliyasdg@gmail.com>
2020-09-07 16:50:55 +0300

    Init

---
+++ Client/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Client
+{
+    class Program
+    {
+        static int port; // порт сервера
+        static string address; // адрес сервера
+        static IPEndPoint ipPoint;
+        static Socket listenSocket;
+        static FileStream file;
+        static FileDetails fileDetails;
+        static string FilesPath;
+        static void Main(string[] args)
+        {
+            Console.Write("Введите IP сервера:");
+            IPAddress iPAddress = IPAddress.Parse(Console.ReadLine());
+            Console.Write("Введите порт:");
+            port = int.Parse(Console.ReadLine());
+            try
+            {
+                listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                ipPoint = new IPEndPoint(iPAddress, port);
+                listenSocket.Connect(ipPoint);
+                Console.WriteLine($"Подключение к серверу прошло успешно: {ipPoint.Address.ToString()}");
+                Console.WriteLine("Введите дирректорию для сохранения файлов");
+                FilesPath = @Console.ReadLine();
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            while (true)
+            {
+                try
+                {
+                    GetFileDetails();
+                    GetFile();
+                }catch(Exception e) { Console.WriteLine(e.Message); }
+            }
+        }
+        static void GetFileDetails()
+        {
+            Console.WriteLine("Ожидание нового отправления...");
+            byte[] buffer=new byte[8192];
+            int size = listenSocket.Receive(buffer);
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(FileDetails));
+            MemoryStream memoryStream = new MemoryStream();
+            memoryStream.Write(buffer, 0, size);
+            memoryStream.Position = 0;
+            fileDetails = (FileDetails)xmlSerializer.Deserialize(memoryStream);
+            memoryStream.Close();
+            Console.WriteLine($"Происходит передача файла {fileDetails.FileType} , который имеет вес {fileDetails.Length} Byte");
+
+        }
+        static void GetFile()
+        {
+            file = new FileStream(FilesPath + fileDetails.FileType,FileMode.Create,FileAccess.ReadWrite,FileShare.ReadWrite);
+            int bufferSize = 8192;
+            byte[] buffer;
+            do
+            {
+                buffer = new byte[bufferSize];
+                int currentSize = listenSocket.Receive(buffer);
+                file.Write(buffer, 0, currentSize);
+                fileDetails.Length -= currentSize;
+
+            } while (fileDetails.Length > 0);
+            file.Close();
+            Console.WriteLine("Файл полностью передан");
+        }
+    }
+
+    [Serializable]
+    public class FileDetails
+    {
+        public string FileType { get; set; }
+        
+        public long Length { get; set; }
+
+    }
+}
---
+++ NET/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Server
+{
+    public class Program
+    {
+        static int port;
+        static string ip;
+
+        static IPEndPoint ipPoint;
+        static Socket listenSocket;
+        static FileStream file;
+        public static FileDetails fileDetails;
+        static void Main(string[] args)
+        {
+            Console.Write("Введите IP сервера:");
+            IPAddress iPAddress = IPAddress.Parse(Console.ReadLine());
+            Console.Write("Введите порт:");
+            port = int.Parse(Console.ReadLine());
+            ipPoint = new IPEndPoint(iPAddress, port);
+            listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket current = null;
+            try
+            {
+                listenSocket.Bind(ipPoint);
+                listenSocket.Listen(1);
+                Console.WriteLine("Сервер запущен...");
+                current = listenSocket.Accept();
+                Console.WriteLine($"Клиент подключен: {current.RemoteEndPoint.ToString()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            while (true)
+            {
+                try
+                {
+                    int bufferSize = 8192;
+                    byte[] buffer = new byte[bufferSize];
+                    Console.WriteLine("Введите путь файла:");
+                    string path = @Console.ReadLine();
+                    file = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+                    SendFileInfo(current);
+
+                    Thread.Sleep(2000);
+
+                    SendFile(current);
+                }
+                catch (FileNotFoundException e) { Console.WriteLine(e.Message); }
+                catch (Exception e) { Console.WriteLine(e.Message); break; }
+
+            }
+        }
+        static void SendFileInfo(Socket current)
+        {
+            fileDetails = new FileDetails();
+            string fullFileName = file.Name;
+            int lastIndex = fullFileName.LastIndexOf(@"\");
+            string type = fullFileName.Remove(0, lastIndex + 1);
+            fileDetails.FileType = type;
+            fileDetails.Length = file.Length;
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(FileDetails));
+            MemoryStream memoryStream = new MemoryStream();
+            xmlSerializer.Serialize(memoryStream, fileDetails);
+            memoryStream.Position = 0;
+            byte[] buffer = new byte[memoryStream.Length];
+            memoryStream.Read(buffer, 0, buffer.Length);
+            memoryStream.Close();
+            current.Send(buffer);
+            Console.WriteLine("Данные о файле отправлены");
+        }
+        static void SendFile(Socket current)
+        {
+            int bufferSize = 8192;
+            byte[] buffer;
+            do
+            {
+                int currentSize = fileDetails.Length >(long) bufferSize ? bufferSize : (int) fileDetails.Length;
+                buffer = new byte[currentSize];
+                file.Read(buffer, 0, currentSize);
+                current.Send(buffer);
+                fileDetails.Length -= currentSize;
+
+            } while (fileDetails.Length > 0);
+            file.Close();
+            Console.WriteLine("Файл отправлен");
+            Thread.Sleep(1000);
+        }
+
+    }
+
+    [Serializable]
+    public class FileDetails
+    {
+        public string FileType { get; set; }
+        public long Length { get; set; }
+        
+    }
+}
